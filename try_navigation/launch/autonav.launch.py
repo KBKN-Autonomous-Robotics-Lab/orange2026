05e5110686e48a9340a8e421d3c6d06fcd6190a5
shozawakoki <koki.shozawa.3i@stu.hosei.ac.jp>
2026-05-09 03:07:29 +0000

    add dbscan to launch

--- try_navigation/launch/autonav.launch.py
+++ try_navigation/launch/autonav.launch.py
@@ -145,14 +145,24 @@ def generate_launch_description():
             output='screen',
             arguments=[],
         ),
-        #takamori Autonav
+        
+        #hatori dbscan
         Node(package='try_navigation',
-            executable='reflection_to_pcd',
-            name='reflection_to_pcd',
+            executable='reflection_to_pcd_dbscan',
+            name='reflection_to_pcd_dbscan',
             output='screen',
             parameters=[{'odom': odom}],
             arguments=[],
         ),
+        
+        #takamori Autonav
+        #Node(package='try_navigation',
+         #   executable='reflection_to_pcd',
+          #  name='reflection_to_pcd',
+           ##parameters=[{'odom': odom}],
+            #arguments=[],
+        #),
+        
         #takamori Selfdrive
         #Node(package='try_navigation',
         #    executable='self_drive_line',
